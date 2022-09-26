d0f4042b95a523846ba87ceeb51ddd93e66aba13
Wilson Loayza <wilson_loayza@hotmail.com>
2022-09-26 07:28:02 +0200

    Correction token

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -12,27 +12,18 @@ export class SpotifyService {
   getQuery(query: string) {
     const url = `https://api.spotify.com/v1/${query}`;
     const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQB0BfULe77HYupiQ0GYb8lfnZ6h4gzTM3l8xA9rL6mo-pQhXfXYU89xV8mQQfcxHKNrqLweEdM8AW6XBetdcuLSWIG1N1fU1UZbpZTWKJbh_PQ3XtUQF2XN7YN8tw9tzrtRVgFOBfU6kzgyq3EnEY89UVLU0dYnCVbLM5FjXpMxoVKR6_gz48xE4U5-vaRHtrzP6NutXzIqaSkwR1c',
+      Authorization: 'Spotify API Token',
     });
     return this.http.get(url, { headers });
   }
 
   getNewReleases() {
-    // const headers = new HttpHeaders({
-    //   Authorization:
-    //     'Bearer BQC3Rgh7VU2lTzt89INLehw7vnuEdq9HBHapx1il0u5AuNNU1B1pGT4gJlDSt6ulsw5ZHoDTSWACC9IUHyc',
-    // });
     return this.getQuery('browse/new-releases').pipe(
       map((data: any) => data['albums'].items)
     );
   }
 
   getArtistas(termino: string) {
-    // const headers = new HttpHeaders({
-    //   Authorization:
-    //     'Bearer BQAalxPjLvrfdtIK-JquYdTQeaiptnOvPfT2lQ3tgYeUzuwVdVwRldpP5vYhbNsx8NdOj5f5SmzHaUQh478',
-    // });
     return this.getQuery(`search?q=${termino}&type=artist&limit=15`).pipe(
       map((data: any) => data['artists'].items)
     );
